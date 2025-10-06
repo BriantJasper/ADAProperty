fd602711e0e758fa79f1d1d4ee83f30775c30f5e
Briant Jasper <briantjasper5@gmail.com>
2025-10-06 19:38:38 +0700

    fixed container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,11 +1,14 @@
-import { ReactNode } from "react";
+import type { ReactNode } from "react";gid
 
 interface ContainerProps {
   children: ReactNode;
   className?: string;
 }
 
-export default function Container({ children, className = "" }: ContainerProps) {
+export default function Container({
+  children,
+  className = "",
+}: ContainerProps) {
   return (
     <div className={`max-w-8xl mx-auto px-4 sm:px-4 lg:px-8 ${className}`}>
       {children}
